dd1f3fb15bade2c325bc79d29e9dd088954937eb
TheOldZoom <theoldzoom@proton.me>
2026-08-23 21:55:40 -0600

    proxy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
   allowedDevOrigins: ["example.com"],
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://samyapi.zoomhub.xyz/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
